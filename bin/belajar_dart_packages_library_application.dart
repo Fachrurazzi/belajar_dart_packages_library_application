07a01e86996e1a3f623097083661ad15eccf7b21
unknown <razzytirta@gmail.com>
2023-07-17 12:00:11 +0800

    Belajar Dart Packages Library Application

---
+++ bin/belajar_dart_packages_library_application.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_packages_library_2023/hello.dart' as belajar;
+
+void main() {
+  print(belajar.sayHello('Razzi'));
+
+  var customer = belajar.Customer();
+  var category = belajar.Category();
+  var product = belajar.Product();
+}
---
+++ test/belajar_dart_packages_library_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_packages_library_application/belajar_dart_packages_library_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
